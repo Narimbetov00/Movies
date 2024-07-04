60c5ccd4c73c810181e6b60d4b7a220ef7ce0845
Narimbetov00 <narimbetov00.mail.ru>
2024-07-04 12:36:25 +0500

    Postgres bazasina movielar salindi

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_object_or_404,redirect
+from .models import *
 # Create your views here.
+
+
+def movie_list(request,category_slug=None):
+    category= None
+    categories = Category.objects.all()
+    movies = Movies.objects.all()
+    if category_slug:
+        category = get_object_or_404(Category,slug = category_slug)
+    return render(request,'movie_list.html',{
+                      'category':category,
+                      'categories':categories,
+                      'movies':movies
+                      })
